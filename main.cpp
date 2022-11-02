c19f3d060ae3cba2b8d678838171cc60db40d769
Youssef Mamdouh <51208752+YoussefMamdouh53@users.noreply.github.com>
2022-11-02 16:42:55 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <SFML/Audio.hpp>
+
 #define ZOMCNT 5
 
 using namespace sf;
@@ -11,9 +13,10 @@ bool gv = false;
 Vector2f charPos = {100,350};
 Vector2f zomPos[5];
 float zomSpd[5];
+bool zomhide[5];
 bool flip[6];
 Vector2i obst[1];
-
+int cnt = 0;
 Vector2f bulletPos = {0,0};
 bool shot = false;
 float shootDir = 0;
@@ -22,13 +25,31 @@ float dy = 0;
 
 int main() {
     app.setFramerateLimit(60);
+    Music bg;
+    if (!bg.openFromFile("background.wav"))
+{
+    return -1;
+}
+    bg.setLoop(true);
+    bg.play();
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("shot.wav"))
+        return -1;
+    Sound sound;
+    sound.setBuffer(buffer);
     Font font;
     font.loadFromFile("arial.ttf");
-    Text gamev;
+    Text gamev, win;
     gamev.setFont(font);
     gamev.setString("Game Over");
     gamev.setCharacterSize(52);
     gamev.setPosition(1900/2 - gamev.getGlobalBounds().width/2, 504/2 - gamev.getGlobalBounds().height/2);
+    gamev.setFillColor(Color::Red);
+    win.setFont(font);
+    win.setString("You Win");
+    win.setCharacterSize(52);
+    win.setPosition(1900/2 - win.getGlobalBounds().width/2, 504/2 - win.getGlobalBounds().height/2);
+
 
     Texture characterT, backgroundT, zombieT, axeT;
     if (!characterT.loadFromFile("char.png")){
@@ -80,6 +101,7 @@ int main() {
         }
 
         if (Keyboard::isKeyPressed(Keyboard::Space)) {
+            sound.play();
             shot = true;
             bulletPos = charPos;
             if (flip[5] == 1)
@@ -113,8 +135,8 @@ int main() {
                 }
             }
             zombie[i].setPosition(zomPos[i]);
-            app.draw(zombie[i]);
-            if (chara.getGlobalBounds().intersects(zombie[i].getGlobalBounds())) {
+            if (!zomhide[i]) app.draw(zombie[i]);
+            if (chara.getGlobalBounds().intersects(zombie[i].getGlobalBounds()) && zomhide[i] == 0) {
                 gv = true;
             }
         }
@@ -123,14 +145,26 @@ int main() {
             bulletPos.x += shootDir * 10;
             axe.setPosition(bulletPos);
             app.draw(axe);
+            for (int i = 0;i<ZOMCNT;i++)
+                if (axe.getGlobalBounds().intersects(zombie[i].getGlobalBounds()) && zomhide[i] == 0) {
+                    zomhide[i] = 1;
+                    shot = false;
+                    cnt--;
+                if (zomhide[i] == 1) cnt++;
+            }
         }
         app.draw(chara);
         }
+        else if (cnt == 5){
+            app.clear();
+            app.draw(win);
+        }
         else {
-                app.clear();
+            app.clear();
             app.draw(gamev);
         }
         app.display();
+        cnt = 0;
     }
     return 0;
 }
